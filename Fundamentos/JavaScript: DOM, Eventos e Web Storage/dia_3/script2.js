59d3eb1809d7f90ed939ea869a7310c74f5af689
Michel <ferrazmichel@gmail.com>
2021-07-08 15:05:27 -0300

    Requisitos 1 e 2

--- Fundamentos/JavaScript: DOM, Eventos e Web Storage/dia_3/script2.js
+++ Fundamentos/JavaScript: DOM, Eventos e Web Storage/dia_3/script2.js
@@ -13,4 +13,21 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dias = document.getElementById("days");
+
+for (let i = 0; i < dezDaysList.length; i += 1) {
+    let dia = document.createElement("li");
+    dia.innerText = dezDaysList[i];
+    dia.className = "day";
+    dias.appendChild(dia); 
+}
+
+let feriado = document.querySelectorAll(".day");
+
+for (let i = 0; i < feriado.length; i += 1) {
+    let check = feriado[i].innerText;
+    if (check == 24 || check == 25 || check == 31) {
+        feriado[i].classList.add("holiday");
+    }
+}
