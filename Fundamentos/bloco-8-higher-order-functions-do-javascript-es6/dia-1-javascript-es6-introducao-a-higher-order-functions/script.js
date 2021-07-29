3fe681b16418dfd1690bff171aea822bb31e3231
Michel <ferrazmichel@gmail.com>
2021-07-29 16:36:39 -0300

    Exercicio 1

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,19 @@
+const funcao = (nome) => {
+  const email = nome.toLowerCase().split(' ').join('_');
+  const cadastro = {
+  nomeCompleto: nome,
+  email: `${email}@trybe.com`,
+  }
+  return cadastro;
+};
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(funcao));
