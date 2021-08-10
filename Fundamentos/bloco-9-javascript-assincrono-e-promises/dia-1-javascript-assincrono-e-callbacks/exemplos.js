25d375418b8c1482d105f42136ed77fdb88b8b7c
Michel <ferrazmichel@gmail.com>
2021-08-10 13:44:36 -0300

    Exemplos

--- Fundamentos/bloco-9-javascript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/exemplos.js
+++ Fundamentos/bloco-9-javascript-assincrono-e-promises/dia-1-javascript-assincrono-e-callbacks/exemplos.js
@@ -1,8 +1,36 @@
-setTimeout(() => {
-  console.log('Comprar parafusos') // Comprar parafusos
-  console.log('Adicionar ao estoque') // Adicionar ao estoque
-}, 2000);
+const assert = require('assert');
 
-console.log('1 - Receber roda'); // 1 - Receber roda
-console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
-console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
+}
+
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
+
+//Exemplo 2
+const userFullName2 = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality2 = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser2 = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    console.log(callback(user));
+  }, delay());
+};
+
+getUser2(userFullName2); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser2(userNationality2); // deve imprimir "Ivan is Russian" depois de um certo tempo
