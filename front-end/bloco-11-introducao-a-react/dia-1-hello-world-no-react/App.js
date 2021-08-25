94c8dedd0937d499f14b6cbb0990f9b4da5aa0a1
Michel <ferrazmichel@gmail.com>
2021-08-25 14:19:04 -0300

    Exercicios

---
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/App.js
@@ -0,0 +1,15 @@
+// const textJSX = <h1>Hello, JSX!</h1>;
+
+// console.log(textJSX);
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
